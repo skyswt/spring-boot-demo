adfca90d12a47d8f95429b6c6192b6f1423896eb
swt <2515767544@qq.com>
2020-10-02 09:59:26 +0800

    Thymeleaf和springboot整合示例

---
+++ src/main/java/edu/zju/cst/spring/boot/controller/IndexController.java
@@ -0,0 +1,43 @@
+package edu.zju.cst.spring.boot.controller;
+
+import edu.zju.cst.spring.boot.bean.PersonBean;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "/thymeleaf")
+public class IndexController {
+
+    @RequestMapping(value = "index", method = RequestMethod.GET)
+    public String index(Model model) {
+        PersonBean person = new PersonBean();
+        person.setName("张三");
+        person.setAge(22);
+
+        List<PersonBean> people = new ArrayList<>();
+        PersonBean p1 = new PersonBean();
+        p1.setName("李四");
+        p1.setAge(23);
+        people.add(p1);
+
+        PersonBean p2 = new PersonBean();
+        p2.setName("王五");
+        p2.setAge(24);
+        people.add(p2);
+
+        PersonBean p3 = new PersonBean();
+        p3.setName("赵六");
+        p3.setAge(24);
+        people.add(p3);
+
+        model.addAttribute("person", person);
+        model.addAttribute("people", people);
+        return "index";
+
+    }
+}
